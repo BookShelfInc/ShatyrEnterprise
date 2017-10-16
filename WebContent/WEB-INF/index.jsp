298e84fd0d14e4efc49d080b5dd676965a720ecb
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-10-16 16:39:02 +0600

    filter

--- WebContent/WEB-INF/index.jsp
+++ WebContent/WEB-INF/index.jsp
@@ -32,6 +32,7 @@
 <c:forEach items="${allPosts}" var="a" varStatus="cnt">
 	<a href='news?page=<c:out value="${a.getId()}"/>'><c:out value="${a.getAddress()}"/></a>
 	<p><c:out value="${a.getPrice()}"/></p>
+	<p><c:out value="${a.getArea()}"/></p>
 	<br>
 </c:forEach>
 
--- src/dao/IPost.java
+++ src/dao/IPost.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import dto.PostDTO;
 
@@ -14,5 +15,10 @@ public interface IPost {
 	public boolean updatePost(PostDTO post) throws SQLException;
 	public boolean deletePost(Long post_id) throws SQLException;
 	
+	public boolean archivePost(Long post_id, boolean val) throws SQLException;
+	
 	public ArrayList<String> getHouseTypes() throws SQLException;
+	
+	public ArrayList<PostDTO> getPostsByFilter(ArrayList<Triplet<Boolean, String, String>> filter) throws SQLException;
+	public ArrayList<PostDTO> getPostsByOrder(String order) throws SQLException;
 }
